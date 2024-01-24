115d3d2a8dd157ce8af2c6cfc52e5061327aa120
rich-s-d <srichnz@gmail.com>
2024-01-24 11:51:51 +0100

    Add mock tests, verfified that pytest is setup and running.

--- app/tests/test_foo.py
+++ app/tests/test_foo.py
@@ -1,5 +1,19 @@
 # app/tests/test_foo.py
 
+import json
+
+from django.urls import reverse
+
+
 def test_hello_world():
     assert "hello_world" == "hello_world"
     assert "foo" != "bar"
+
+# Code wisperer, please explain the steps in the following function
+    
+def test_ping(client):
+    url = reverse("ping")
+    response = client.get(url)
+    content = json.loads(response.content)
+    assert response.status_code == 200
+    assert content["ping"] == "pong!"
