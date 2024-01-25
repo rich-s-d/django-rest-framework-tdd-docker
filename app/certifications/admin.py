fd9ccbed901f4a735126b19b8693600f2009b644
rich-s-d <srichnz@gmail.com>
2024-01-25 10:06:31 +0100

    update admin for certification

--- app/certifications/admin.py
+++ app/certifications/admin.py
@@ -1,3 +1,18 @@
+# app/certifications/admin.py
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Certification
+
+
+@admin.register(Certification)
+class CertificationAdmin(admin.ModelAdmin):
+    fields = (
+        "certification", "issuer", "year_obtained", "created_date", "cert_expiry_date",
+    )
+    list_display = (
+        "certification", "issuer", "year_obtained", "created_date", "cert_expiry_date",
+    )
+    readonly_fields = (
+        "created_date",
+    )
