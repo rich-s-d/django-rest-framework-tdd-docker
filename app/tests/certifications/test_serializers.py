91ea1b715a401360eccdf8bd7d195f1de71da9ce
rich-s-d <srichnz@gmail.com>
2024-01-25 10:07:32 +0100

    tests for certification

---
+++ app/tests/certifications/test_serializers.py
@@ -0,0 +1,30 @@
+# app/tests/certifications/test_serializers.py
+
+from certifications.serializers import CertificationSerializer
+
+
+def test_valid_certification_serializer():
+    valid_serializer_data = {
+        "certification": "AWS Cloud Practioner",
+        "issuer": "AWS",
+        "year_obtained": "1987",
+        "cert_expiry_date": "2026"
+    }
+    serializer = CertificationSerializer(data=valid_serializer_data)
+    assert serializer.is_valid()
+    assert serializer.validated_data == valid_serializer_data
+    assert serializer.data == valid_serializer_data
+    assert serializer.errors == {}
+
+
+def test_invalid_certification_serializer():
+    invalid_serializer_data = {
+        "certification": "AWS Cloud Practioner",
+        "issuer": "AWS",
+        "year_obtained": "1987",
+    }
+    serializer = CertificationSerializer(data=invalid_serializer_data)
+    assert not serializer.is_valid()
+    assert serializer.validated_data == {}
+    assert serializer.data == invalid_serializer_data
+    assert serializer.errors == {"cert_expiry_date": ["This field is required."]}
---
+++ app/tests/certifications/test_views.py
@@ -0,0 +1,91 @@
+# app/tests/certifications/test_views.py
+
+import json
+
+import pytest
+
+from certifications.models import Certification
+
+
+@pytest.mark.django_db
+def test_add_movie(client):
+    certifications = Certification.objects.all()
+    assert len(certifications) == 0
+
+    resp = client.post(
+        "/api/certifications/",
+        {
+            "certification": "AWS Certifified DevOps Professional",
+            "issuer": "AWS",
+            "year_obtained": "2023",
+            "cert_expiry_date": "2026"
+        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 201
+    assert resp.data["certification"] == "AWS Certifified DevOps Professional"
+
+    certifications = Certification.objects.all()
+    assert len(certifications) == 1
+
+
+@pytest.mark.django_db
+def test_add_certification_invalid_json(client):
+    certifications = Certification.objects.all()
+    assert len(certifications) == 0
+
+    resp = client.post(
+        "/api/certifications/",
+        {},
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    certifications = Certification.objects.all()
+    assert len(certifications) == 0
+
+
+@pytest.mark.django_db
+def test_add_certification_invalid_json_keys(client):
+    certifications = Certification.objects.all()
+    assert len(certifications) == 0
+
+    resp = client.post(
+        "/api/certifications/",
+        {
+            "certification": "AWS Certifified DevOps Professional",
+            "issuer": "AWS",
+            "year_obtained": "2023",        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    certifications = Certification.objects.all()
+    assert len(certifications) == 0
+
+
+@pytest.mark.django_db
+def test_get_single_certification(client, add_certification):
+    certification = add_certification(certification="AWS Certifified DevOps Professional",
+                              issuer="AWS",
+                              year_obtained="2023",
+                              cert_expiry_date="2026")
+    resp = client.get(f"/api/certifications/{certification.id}/")
+    assert resp.status_code == 200
+    assert resp.data["certification"] == "AWS Certifified DevOps Professional"
+
+    
+@pytest.mark.django_db
+def test_get_all_certifications(client, add_certification):
+    certification_one = add_certification(certification="AWS Certifified DevOps Professional",
+                              issuer="AWS",
+                              year_obtained="2023",
+                              cert_expiry_date="2026")
+    certification_two = add_certification(certification="AWS Certifified Developer Associate",
+                              issuer="AWS",
+                              year_obtained="2023",
+                              cert_expiry_date="2026")
+    resp = client.get(f"/api/certifications/")
+    assert resp.status_code == 200
+    assert resp.data[0]["certification"] == certification_one.certification
+    assert resp.data[1]["certification"] == certification_two.certification
