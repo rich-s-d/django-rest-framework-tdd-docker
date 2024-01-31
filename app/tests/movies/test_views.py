f5a03b9cbe749bad1e7dc6f6b20cc26c8957a181
rich-s-d <srichnz@gmail.com>
2024-01-31 11:53:39 +0100

    Fix flake8 comments.

--- app/tests/movies/test_views.py
+++ app/tests/movies/test_views.py
@@ -100,7 +100,7 @@ def test_remove_movie(client, add_movie):
 
 @pytest.mark.django_db
 def test_remove_movie_incorrect_id(client):
-    resp = client.delete(f"/api/movies/99/")
+    resp = client.delete("/api/movies/99/")
     assert resp.status_code == 404
 
 
@@ -125,15 +125,16 @@ def test_update_movie(client, add_movie):
 
 @pytest.mark.django_db
 def test_update_movie_incorrect_id(client):
-    resp = client.put(f"/api/movies/99/")
+    resp = client.put("/api/movies/99/")
     assert resp.status_code == 404
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize("add_movie, payload, status_code", [
     ["add_movie", {}, 400],
-    ["add_movie",{"title": "The Big Lebowski", "genre": "comedy"}, 400],
+    ["add_movie", {"title": "The Big Lebowski", "genre": "comedy"}, 400],
 ], indirect=["add_movie"])
+# indirect is used to parametrize the fixture, add_movie
 def test_update_movie_invalid_json(client, add_movie, payload, status_code):
     movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
     resp = client.put(
