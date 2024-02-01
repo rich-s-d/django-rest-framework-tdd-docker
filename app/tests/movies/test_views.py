d55007dc95b15fea4e38c16bf3999e285f82260b
rich-s-d <srichnz@gmail.com>
2024-02-01 18:45:56 +0100

    add unit tests for API View

--- app/tests/movies/test_views.py
+++ app/tests/movies/test_views.py
@@ -111,7 +111,7 @@ def test_update_movie(client, add_movie):
     resp = client.put(
         f"/api/movies/{movie.id}/",
         {"title": "The Big Lebowski", "genre": "comedy", "year": "1997"},
-        content_type="application/json"
+        content_type="application/json",
     )
     assert resp.status_code == 200
     assert resp.data["title"] == "The Big Lebowski"
@@ -130,10 +130,14 @@ def test_update_movie_incorrect_id(client):
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("add_movie, payload, status_code", [
-    ["add_movie", {}, 400],
-    ["add_movie", {"title": "The Big Lebowski", "genre": "comedy"}, 400],
-], indirect=["add_movie"])
+@pytest.mark.parametrize(
+    "add_movie, payload, status_code",
+    [
+        ["add_movie", {}, 400],
+        ["add_movie", {"title": "The Big Lebowski", "genre": "comedy"}, 400],
+    ],
+    indirect=["add_movie"],
+)
 # indirect is used to parametrize the fixture, add_movie
 def test_update_movie_invalid_json(client, add_movie, payload, status_code):
     movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
